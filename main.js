f7e6963bd00ce3449ed7b13f8e2dcaba2db00fd0
Dylan K <50585244+dylankainth@users.noreply.github.com>
2022-08-04 14:39:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,21 +19,6 @@ class ScratchFetch {
                                 },
                             }
                         },
-                        {
-                            "opcode": "jsonExtract",
-                            "blockType": "reporter",
-                            "text": "extract [name] from [data]",
-                            "arguments": {
-                                "name": {
-                                    "type": "string",
-                                    "defaultValue": "temperature"
-                                },
-                                "data": {
-                                    "type": "string",
-                                    "defaultValue": '{"temperature": 12.3}'
-                                },
-                            }
-                        },
                 ],
         };
     }
